ba7bafa76b06cb777f98b8b4acb2a04ac1da3ce2
Adam-Smith-Coder <adcollects@gmail.com>
2022-07-23 10:56:11 +0100

    Update script.js with first math function
    Create basic function to get the square root of a number
    Add an If/Else statement to do a check if input isNAN, where it is
    not a num then log an error
    Where it is a num, add the square root function to this to separate them

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getVal() {
+let getVal = function() {
     const val = document.querySelector('input').value;
-    console.log(val);
+    if (isNaN(val)) {
+        console.log('ERROR')
+    } else {
+        console.log(val);
+        squareRoot(val);
+    } 
+}
+
+function squareRoot(num) {
+    let sqrRoot = Math.sqrt(num);
+    console.log(`the square root of ${num} is ${sqrRoot}`)
 }
