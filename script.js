fa62699fb8fa78cc0be63a22e719af3c3183714f
Adam-Smith-Coder <adcollects@gmail.com>
2022-07-25 16:28:34 +0100

    Update script.js with next math function
    Create a function to work out if a number is prime or not

--- script.js
+++ script.js
@@ -5,10 +5,32 @@ let getVal = function() {
     } else {
         console.log(val);
         squareRoot(val);
+        prime(val);
     } 
 }
 
 function squareRoot(num) {
     let sqrRoot = Math.sqrt(num);
     console.log(`the square root of ${num} is ${sqrRoot}`)
+}
+
+let prime = function(val) {
+    if (isPrime(val) === true) {
+        console.log(`${val} is a Prime Number`)
+    } else {
+        console.log(`${val} is not a Prime Number`)
+    }
+}
+
+function isPrime(num) {
+    if (num < 2) {
+        return false
+    }
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false
+        }
+    }
+    return true    
 }
